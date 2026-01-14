8d6a51b787dcb0715883395176f7fc976d01e782
Aman Deep <artemisfowl795@gmail.com>
2026-01-14 09:20:34 +0530

    Add baseline Logistic Regression model with evaluation metrics

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from src.data.data_loader import DataLoader
+
 
 from src.utils.logger import logger
 from src.utils.exceptions import FraudException
+#___________________________________________________________________________________
+
+from src.data.data_loader import DataLoader
 
 if __name__== "__main__":
   try:
@@ -15,13 +18,20 @@ if __name__== "__main__":
   except Exception as e:
     logger.error("Pipeline Failed")
     raise FraudException("Main Pipeline Failed")
-  
+#___________________________________________________________________________________
 from src.preprocessing.preprocessor  import Preprocessor
 
 preprocessor=Preprocessor()
 
 X,y= preprocessor.split_features_target(df)
-
 X_train, X_test, y_train, y_test =preprocessor.train_test_split_data(X, y)
-
 logger.info("Preprocessing step completed successfully")
+
+#___________________________________________________________________________________
+from src.models.logistic_regression import LogisticRegressionTrainer
+trainer= LogisticRegressionTrainer()
+
+model=trainer.train(X_train, y_train)
+
+report, auc=trainer.evaluate(X_test, y_test)
+logger.info("Baseline Logistic Regression COmpleted")
